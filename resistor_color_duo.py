3b826540d5d5de6e55e72272e089974f05c93c47
Gamattowicz <p.romanczuk31@gmail.com>
2021-03-11 12:52:52 +0100

    add solution to "Resistor Color Duo" exercise

---
+++ resistor_color_duo.py
@@ -0,0 +1,8 @@
+def value(colors):
+    code = {'black': 0, 'brown': 1, 'red': 2, 'orange': 3, 'yellow': 4, 'green': 5, 'blue': 6, 'violet': 7, 'grey': 8,
+            'white': 9}
+    result = ''
+    for color in colors[:2]:
+        result += str(code[color])
+    return int(result)
+
