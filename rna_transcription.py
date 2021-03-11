51bf4d454bdba244e0c62dc0be8260a19b5c6ae8
Gamattowicz <p.romanczuk31@gmail.com>
2021-03-11 12:13:41 +0100

    add one-liner solution to "RNA Transcription" exercise

--- rna_transcription.py
+++ rna_transcription.py
@@ -1,13 +1,3 @@
 def to_rna(dna_strand):
-    result = ''
-    for i in dna_strand:
-        if i == 'G':
-            result += 'C'
-        elif i == 'C':
-            result += 'G'
-        elif i == 'T':
-            result += 'A'
-        elif i == 'A':
-            result += 'U'
-    return result
+    return dna_strand.translate(str.maketrans('GCTA', 'CGAU'))
 
