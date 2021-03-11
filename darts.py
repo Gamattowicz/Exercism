d68cd3adb3b189dff9238b9f7c5ea1c0fd523542
Gamattowicz <p.romanczuk31@gmail.com>
2021-03-11 15:37:02 +0100

    add solution to "Darts" exercise

---
+++ darts.py
@@ -0,0 +1,15 @@
+from math import pow, sqrt
+
+
+def score(x, y):
+    radius = sqrt(pow(x, 2) + pow(y, 2))
+
+    if radius <= 1:
+        return 10
+    elif radius <= 5:
+        return 5
+    elif radius <= 10:
+        return 1
+    return 0
+
+print(score(5, 5))
