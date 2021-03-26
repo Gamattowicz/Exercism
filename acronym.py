cb49cc13a245ce1f08560d0872365de301f36c00
Gamattowicz <p.romanczuk31@gmail.com>
2021-03-26 13:58:51 +0100

    add shorter solution to "Acronym" exercise

--- acronym.py
+++ acronym.py
@@ -2,8 +2,5 @@ import re
 
 
 def abbreviate(words):
-    result = ''
-    for word in re.split(r"[-_\s]+", words):
-        if word[0].isalpha():
-            result += word[0].upper()
-    return result
+    return ''.join([word[0].upper() for word in re.split(r"[-_\s]+", words)
+                    if word[0].isalpha()])
