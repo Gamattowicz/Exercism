b3f965f3bfda281eb314ea2e4434d4c73a79cd4d
Gamattowicz <p.romanczuk31@gmail.com>
2021-03-27 20:44:14 +0100

    add shorter solution to "Kindergarten Garden" exercise

--- kindergarten_garden.py
+++ kindergarten_garden.py
@@ -1,26 +1,19 @@
-plants = {
-    'G': 'Grass',
-    'C': 'Clover',
-    'R': 'Radishes',
-    'V': 'Violets'
-}
-
-
 class Garden:
-    def __init__(self, diagram, students=['Alice', 'Bob', 'Charlie',
-                                          'David', 'Eve', 'Fred', 'Ginny',
-                                          'Harriet', 'Ileana', 'Joseph',
-                                          'Kincaid', 'Larry']):
+    seeds = {
+        'G': 'Grass',
+        'C': 'Clover',
+        'R': 'Radishes',
+        'V': 'Violets'
+    }
+    children = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred', 'Ginny',
+                'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry']
+
+    def __init__(self, diagram, students=children):
         self.diagram_1, self.diagram_2 = diagram.split('\n')
-        students.sort()
-        self.students = students
+        self.students = sorted(students)
 
     def plants(self, student):
-        index = self.students.index(student)
-        if index != 0:
-            index *= 2
-        result = []
-        for plant in (self.diagram_1[index:index+2] + self.diagram_2[
-                                               index:index+2]):
-            result.append(plants[plant])
-        return result
+        index = self.students.index(student) * 2
+
+        return [self.seeds[plant] for plant in (self.diagram_1[index:index+2] +
+                                                self.diagram_2[index:index+2])]
