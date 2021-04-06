00250d0a74950e88b6188b34bb2888afae468879
Gamattowicz <p.romanczuk31@gmail.com>
2021-04-06 18:49:35 +0200

    add shorter solution to "Largest Series Product" exercise

--- largest_series_product.py
+++ largest_series_product.py
@@ -2,11 +2,9 @@ from math import prod
 
 
 def largest_product(series, size):
-    if size < 0:
+    if size < 0 or size > len(series):
         raise ValueError('Size must be greater or equal to 0')
     products = []
-    result = []
     for i in range(len(series) - size + 1):
         products.append([int(num) for num in series[i: i+size]])
-    result.append([prod(i) for i in products])
-    return max(result[0])
+    return max([prod(i) for i in products])
