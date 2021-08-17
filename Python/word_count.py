2d4aeb05bf89d24bab3635b66e55bef5d0a1c6c8
Gamattowi <p.romanczuk31@gmail.com>
2021-08-17 20:57:23 +0200

    move solutions to separate directories

--- Python/word_count.py
+++
@@ -1,7 +0,0 @@
-from collections import Counter
-import re
-
-
-def count_words(sentence):
-    return Counter([word.strip("'") for word in re.findall("[A-Za-z0-9']+",
-                                                           sentence.lower())])
